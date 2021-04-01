3263111ffa6c5eb790956971ab680a66538e8632
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-04-01 17:18:49 -0300

    24min

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
 
 function Header() {
-    return <div className="header">
-        <h2> I am the header </h2>
-    </div>
+    return (
+        <div className="header">
+            <h2> I am the header </h2>
+            <PersonIcon />
+        </div>
+    )
 }
 
 export default Header;
