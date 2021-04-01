ccf671852f2d732f327c06788af459de4b1d52c6
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-04-01 14:54:36 -0300

    2nd commit

---
+++ tinder-clone/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return <div className="header">
+        <h2> I am the header </h2>
+    </div>
+}
+
+export default Header;
